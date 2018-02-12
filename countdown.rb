c67143e2e9d2f6a1c90b5d96585b4a77918b7a16
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 17:37:30 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown(number)
-  while  number.to_i > 0
-    puts "#{number}"
+  while number.to_i > 0
+    puts "-#{number} second(s)"
     number -= 1
   end
   "HAPPY NEW YEAR!"
